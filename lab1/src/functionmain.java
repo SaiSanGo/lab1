0dec480f1dab80a20d0dc58a5a9b57c6ab3bf21f
SaiSanGo <532706564@qq.com>
2016-10-15 11:08:07 +0800

    add lab1

--- lab1/src/functionmain.java
+++ lab1/src/functionmain.java
@@ -2,6 +2,7 @@ import java.util.*;
 import java.io.*;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
+// The line was added just now!
 public class functionmain{
 	private static String[] temp;
 	public static void main(String[] args)
